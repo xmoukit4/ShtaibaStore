4371c8109d1fa5e9a99effca7217b5b89cac0a0c
Mustapha <xmoukit4@gmail.com>
2022-03-08 15:56:26 +0100

    .

--- admin/admin.js
+++ admin/admin.js
@@ -4,16 +4,11 @@ const closeBtn = document.querySelector("#closeBtn");
 const page = document.querySelector(".container");
 const main = document.querySelector("#wholepage");
 
-
-
-
-
 menuBtn.addEventListener(
     'click',()=>{
         sideMenu.style.display='block';
     }
 )
-
 
 closeBtn.addEventListener(
     'click',()=>{
@@ -27,9 +22,7 @@ Orders.forEach(order => {
     <td>${order.productNumber}</td>
     <td>${order.paymentStatus}</td>
     <td class ="danger">${order.shipping}</td>
-    <td class ="primary">Details</td>
-
-    `
+    <td class ="primary">Details</td>`
     tr.innerHTML = trContent;
     document.querySelector('table tbody').appendChild(tr);
 })
