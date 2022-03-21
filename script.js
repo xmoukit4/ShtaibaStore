cacf61a3d3a4a9a1bdc130670ad2c1dbf4f67eef
sanaeAitbrik <102074940+sanaeAitbrik@users.noreply.github.com>
2022-03-21 20:04:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var compteurImage=1;
+var totalimage=2;
+
+function sliderAuto()
+{
+     var image=document.getElementById('1');
+     image.src= "./photos/fashion" + compteurImage + ".jpg" ;
+     
+     if(compteurImage>=totalimage)
+     {
+        compteurImage=1
+     }
+    else{
+      compteurImage=compteurImage + 1 ;
+    }
+    
+    
+}
+
+window.setInterval(sliderAuto,30000) ;
